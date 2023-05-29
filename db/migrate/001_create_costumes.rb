f3fed1822e055d1c4948f4c33ff7e2ecfb8883d9
nicole <nicolenamayan133@gmail.com>
2023-05-29 17:27:07 +0300

    done

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,11 @@
+class CreateCostumesTable < ActiveRecord::Migration[4.2]
+  def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :image_url
+        t.string :size
+        t.timestamps
+      end
+    end
+end
---
+++ db/migrate/002_create_costume_stores.rb
@@ -0,0 +1,13 @@
+class CreateCostumeStoresTable < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+          t.string :name
+          t.string :location
+          t.integer :costume_inventory
+          t.integer :num_of_employees
+          t.boolean :is_in_business
+          t.datetime :opening_time
+          t.datetime :closing_time
+        end
+      end
+end
